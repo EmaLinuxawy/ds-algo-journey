b3712483ffda544d71d30b5b32014c868d9cc128
EmaLinuxawy <emalinuxawy@gmail.com>
2023-06-02 12:55:27 -0600

    adding BST

---
+++ binary-search-tree/insert.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+
+        if self.root is None:
+            self.root = new_node
+            return True
+        
+        temp = self.root
+
+        while (True):
+            if new_node.value == temp.value:
+                return False
+
+            if new_node.value < temp.value:
+                if temp.left == None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right == None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+
+print(my_tree.root.value)
+print(my_tree.root.left.value)
+print(my_tree.root.right.value)
